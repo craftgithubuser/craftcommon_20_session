2edde68736405617645cf1e994ee05be4b371c96
AlemyeAlemye <alemsegedgebru85@gmail.com>
2023-07-24 02:19:19 -0400

    updating

---
+++ Eth1/done/JavaSampleproject/src/com/selam/Example1.java
@@ -0,0 +1,12 @@
+package com.selam;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.print("Hello World");
+  System.out.print("Hello World");
+  
+	}
+
+}
